97795a7d8a482723429d5e5a8293bb42aa2d8363
Brando Lugo <brando.lugo@gmail.com>
2020-12-17 13:09:28 -0600

    Initial Commit

---
+++ Shared/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  Shared
+//
+//  Created by Brando Lugo on 12/17/20.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
