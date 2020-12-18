fc175144dec634120b3d842016b01602b3a9c29c
Brando Lugo <brando.lugo@gmail.com>
2020-12-18 11:56:06 -0600

    Initial project setup

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    
+    var correctAnswer = Int.random(in: 0...2)
+    
+    
+    @State private var showingAlert = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.1, green: 0.1, blue: 0.3).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                    Text(countries[correctAnswer])
+                }
+                    .foregroundColor(.white)
+                ForEach(0..<3){ number in
+                    Button(action: {
+                        //Flag tapped
+                    }, label: {
+                        Image(self.countries[number])
+                            .renderingMode(.original)     
+                    })
+                }
+            }
+        }
     }
 }
 
